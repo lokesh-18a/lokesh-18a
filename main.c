3caee7899b48cbbb5215df1d7318d434252e0360
kemidilokesh <kemidilokesh@gmail.com>
2024-07-06 18:02:53 +0000

    Added Pin to every function for added security. Will complete it by August 15 2024

--- main.c
+++ main.c
@@ -8,6 +8,8 @@ struct bankAccount{
     char accountHolder[100];
     float balance;
     long int accountNo;
+    int pin;
+    int hasPin;
 };
 // Function to create a new bank account
 void createAccount(struct bankAccount accounts[],int numAccounts){
@@ -29,32 +31,39 @@ void createAccount(struct bankAccount accounts[],int numAccounts){
 }
 // Function for pin creation for deposits and withdrawals
 void pinCreation(struct bankAccount accounts[],int numAccounts){
-    int key,pin;
+    int key,pin,temp;
     long int accountno;
     printf("Are you sure to create pin?\n");
     printf("Enter 0 for No and 1 for Yes\n");
     scanf("%d",&key);
     if(key){
-        scan : printf("Enter Account number:");
+        int accountFound = 0;
+        printf("Enter Account number:");
         scanf("%ld",&accountno);
         for(int i=0;i<numAccounts;i++){
             if(accounts[i].accountNo == accountno){
+                accountFound = 1;
                 printf("Account Found Successfully!\n");
                 printf("Enter the 4 digit password\n");
                 scanf("%d",&pin);
-            }
-            else{
-                printf("Account Not Found!\n");
-                printf("Try Again!\n");
-                goto scan;
+                while(pin < 1000 || pin > 9999){
+                    printf("Invalid PIN. Please enter a 4-digit number: ");
+                    scanf("%d",&pin);
+                }
+                printf("Please Enter the PIN Again!\n");
+                scanf("%d",&temp);
+                while(temp!=pin){
+                    printf("PINs do not match. Please Enter the PIN Again!\n");
+                    scanf("%d",&temp);
+                }
+                printf("PIN created successfully!\n");
+                break;
             }
         }
-        int temp;
-        while(temp!=pin){
-            printf("Please Enter the PIN Again!\n");
-            scanf("%d",&temp);
+        if(!accountFound){
+            printf("Account Not Found!\n");
+            printf("Try Again!\n");
         }
-        printf("PIN created successfully!\n");
     }
 }
 
@@ -64,12 +73,10 @@ void deposit(struct bankAccount accounts[],int numAccounts,long int accountNumbe
         if(accountNumber == accounts[i].accountNo){
             accounts[i].balance += amount;
             printf("Deposit Successful. New Balance = %.4f\n",accounts[i].balance);
-        }
-        else{
-            printf("Account not found. Deposit Failed!.\n");
+            return;
         }
     }
-    
+    printf("Account not found. Deposit Failed!.\n");
 }
 
 /* Function to perform a withdrawal from a Bank Account*/
@@ -79,9 +86,11 @@ void withdrawal(struct bankAccount accounts[],int numAccounts,long int accountNu
             if(amount <= accounts[i].balance){
                 accounts[i].balance -= amount;
                 printf("Withdrawal Successful. New Balance = %.4f\n",accounts[i].balance);
+                return;
             }
             else{
                 printf("Insufficient funds. Withdrawal failed!!\n");
+                return;
             }
         }
         else{
@@ -97,16 +106,15 @@ void check(const struct bankAccount accounts[], int numAccounts,long int account
             printf("Account Holder : %s\nAccount Number : %ld\nBalance : %.4f\n",accounts[i].accountHolder,accounts[i].accountNo,accounts[i].balance);
             return;
         }
-        else{
-            printf("Account not found. Unable to check balance.\n");
-        }
     }
-    
+    else{
+        printf("Account not found. Unable to check balance.\n");
+    }
 }
 
 int main(){
     struct bankAccount accounts[MAX_ACCTS];
-    int numAcccounts = 0;
+    int numAccounts = 0;
     int choice;
     long int accountNumber;
     float amount;
@@ -122,29 +130,29 @@ int main(){
         scanf("%d",&choice);
         switch(choice){
             case 1:
-                   createAccount(accounts, numAcccounts);
+                   createAccount(accounts, numAccounts);
                    break;
             case 2:
-                   pinCreation(accounts,numAcccounts);
+                   pinCreation(accounts,numAccounts);
                    break;
             case 3:
                    printf("Enter Account Number for deposit : ");
                    scanf("%ld",&accountNumber);
                    printf("Enter Deposit amount : ");
                    scanf("%f",&amount);
-                   deposit(accounts,numAcccounts,accountNumber,amount);
+                   deposit(accounts,numAccounts,accountNumber,amount);
                    break;
             case 4:
                    printf("Enter Account Number for withdrawal : ");
                    scanf("%ld",&accountNumber);
                    printf("Enter Withdrawal amount : ");
                    scanf("%f",&amount);
-                   withdrawal(accounts,numAcccounts,accountNumber,amount);
+                   withdrawal(accounts,numAccounts,accountNumber,amount);
                    break;
             case 5: 
                    printf("Enter Account Number to check Balance : ");
                    scanf("%ld",&accountNumber);
-                   check(accounts,numAcccounts,accountNumber);
+                   check(accounts,numAccounts,accountNumber);
                    break;
             case 6:
                    printf("Exiting the Bank. Thank You!\n");
